e87c1274bf5ad7f8ebd8088e7abaf6457f5d714e
Alec Luro <alec.b.luro@gmail.com>
2020-07-16 14:11:01 -0500

    updated posterior fits plot

--- Scripts/04_UPDATED_Nonlinear_Model.R
+++ Scripts/04_UPDATED_Nonlinear_Model.R
@@ -11,6 +11,8 @@ library(tidybayes)
 library(modelr)
 library(extrafont)
 library(ggstance)
+library(ggfortify)
+library(LaplacesDemon)
 
 # Load data
 data <-
@@ -24,32 +26,32 @@ data <-
 
 # Full model ------------------------------------------------------------
 
-
 priors_nl <- c(
   prior(normal(0, 3), nlpar = "eta"),
   prior(student_t(10, 1, 1.5), nlpar = "eta", class = "b", coef = "intersquare_dist_mm")
 )
 
+# Plot priors
 ggfortify::ggdistribution(LaplacesDemon::dst, seq(-5, 5, 0.01),  nu = 10, mu = 1, sigma = 1.5, fill = "blue")
 ggfortify::ggdistribution(dnorm, seq(-5, 5, 0.01),  mean = 0, sd  = 4 ,  fill = "blue")
 
 ## Include asymptotes
-## lower = 20% (20% rejection rate for Cowbird-sized eggs painted a blue-green American robin mimetic color )
+## lower = 15% (~15% rejection rate for Cowbird-sized eggs painted a blue-green American robin mimetic color )
 ##  source DOI: 10.1002/ece3.3759
-## upper = 80% (No eggs with "unnatural" colors were rejected at rates higher than 80%--e.g., blue, red and yellow painted Cowbird-sized eggs)
-## source DOI: 10.1007/s00265-013-1649-8
+## upper = 85% (No eggs with "unnatural" colors were rejected at rates higher than 80%--e.g., blue, red and yellow painted Cowbird-sized eggs)
+##  source DOI: 10.1007/s00265-013-1649-8
 
 #inv_logit <- function(x) 1 / (1 + exp(-x))
 
-model_rejection_nl_prior<- 
+model_rejection_nl<- 
   brm(
-    bf(reject_egg ~ 0.20 + 0.80 * inv_logit(eta),
+    bf(reject_egg ~ 0.15 + 0.85 * inv_logit(eta),
        eta ~ intersquare_dist_mm,
        nl = TRUE),
     data = data,
-    #iter = 10000,
-    #chains = 4,
-    #cores = 6,
+    iter = 10000,
+    chains = 4,
+    cores = 6,
     family = bernoulli("identity"), 
     prior = priors_nl,
     seed = 868,
@@ -57,24 +59,85 @@ model_rejection_nl_prior<-
     save_all_pars = T
 )
 
+saveRDS(model_rejection_nl, "Results/UPDATED_model_rejection_nl.RDS")
+
+model_rejection_nl <-
+  readRDS("Results/UPDATED_model_rejection_nl.RDS")
+
+## Prior-only
+model_rejection_nl_prior<- 
+  brm(
+    bf(reject_egg ~ 0.15 + 0.85 * inv_logit(eta),
+       eta ~ intersquare_dist_mm,
+       nl = TRUE),
+    data = data,
+    iter = 10000,
+    chains = 4,
+    cores = 6,
+    family = bernoulli("identity"), 
+    prior = priors_nl,
+    seed = 868,
+    sample_prior = "only",
+    save_all_pars = T
+  )
+
 saveRDS(model_rejection_nl_prior, "Results/UPDATED_model_rejection_nl_prior.RDS")
 
-plot(conditional_effects(model_rejection_nl_prior, probs = c(0.05, 0.95), spaghetti = F), points = T)
+model_rejection_nl_prior <-
+    readRDS("Results/UPDATED_model_rejection_nl_prior.RDS")
 
 # Plot posterior predictive check for discrete response
-pdf("Figures/supp_ppcheck_rootogram.pdf")
+cairo_pdf("Figures/supp_ppcheck_rootogram.pdf")
 
-pp_check(model_rejection_nl_prior, type = "rootogram")
+pp_check(model_rejection_nl_prior, type = "rootogram") +
+  labs(title = "Prior Predictive Check") +
+  theme(text = element_text(size = 12),
+        plot.title = element_text(hjust = 0.5))
 
 dev.off()
 
 # Traceplot
-pdf("Figures/supp_traceplot.pdf")
+cairo_pdf("Figures/supp_traceplot.pdf")
 
 plot(model_rejection_nl)
 
 dev.off()
 
-# Bayes R2
-bayes_R2(model_rejection_nl_prior)
+# model_rejection_nl fit ------------------------------------------
+pp_check(model_rejection_nl,
+         type = "rootogram",
+         nsamples = 100)
+
+# R2[Error] = 0.10 [0.09]
+bayes_R2(model_rejection_nl, robust = T)
+
+## Highest Density interval of interquare_dist_mm
+
+# Posterior Median +- MAD [95% Credible Interval] = 1.29 +- 0.75 [-0.07, 2.97]
+## brms
+posterior_summary(model_rejection_nl, 
+                  robust = T)
+
+## tidybayes
+model_rejection_nl %>% 
+  spread_draws(b_eta_intersquare_dist_mm) %>%
+  median_hdci()
+
+
+# Get rejection threshold (inter-square distance where rejection prob = 0.5) --------------------
+
+# treshold +- error [75% Credible Interval] = 1.357mm +- 0.5mm [0.833, 1.899]
+data %>%
+  data_grid(
+    intersquare_dist_mm = seq_range(intersquare_dist_mm, by = 0.001),
+  ) %>%
+  add_fitted_draws(model_rejection_nl, n = 1000, scale = "response", seed = 10) %>%
+  mutate(.value = round(.value, 2)) %>% 
+  ## Filter to narrow down to values where rejection probability = 0.5
+  filter(.value == 0.5) %>% 
+  select(intersquare_dist_mm) %>%
+  as_tibble() %>% 
+  as.mcmc() %>% 
+  ## Get posterior median and 75% credible interval
+  posterior_summary(robust = T, probs = c(0.15, 0.85))
 
---
+++ Scripts/05_UPDATED_Fig3_nl_model_fitted_probabilities.R
@@ -0,0 +1,92 @@
+# Load packages -----------------------------------------------------------
+library(magrittr)
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(janitor)
+library(brms)
+library(tidybayes)
+library(modelr)
+library(extrafont)
+library(flextable)
+library(ggstance)
+
+
+# Load Data and Model -----------------------------------------------------
+
+## brms model
+model_rejection_nl <-
+  readRDS("Results/UPDATED_model_rejection_nl.RDS")
+
+# Plot model --------------------------------------------------------------
+
+## Add model-fitted posterior draws
+plot_data <-
+  model_rejection_nl$data %>%
+  data_grid(
+    intersquare_dist_mm = seq_range(intersquare_dist_mm, by = 0.1)
+  ) %>%
+  add_fitted_draws(model_rejection_nl, n = 10000, scale = "response", seed = 10) 
+
+## Get median-fitted values for draws
+
+median_fit <-
+  plot_data %>% 
+  group_by(intersquare_dist_mm) %>% 
+  mutate(median_val = median(.value)) %>% 
+  ungroup()
+
+
+## Plot model
+
+plot <-
+ggplot(median_fit, aes(x = intersquare_dist_mm, y = reject_egg)) +
+  ## model fits
+  stat_smooth(aes(y = .value, group = .draw), geom = "line", size = 0.5, alpha = 0.01, color = "#22BDAD", se = FALSE) +
+  ## global model fit
+  geom_path(aes(y = median_val), size = 1.5, alpha = 1, color = "black") +
+  ## 0.5 rejection probability threshold
+  geom_segment(x = 1.357, xend = 1.357, y = 0, yend = 0.5,
+               color = "red",
+               alpha = 0.3,
+               size = 1.5,
+               linetype = "dotted") +
+  geom_pointrangeh(aes(y = 0.5, x = 1.357,  xmin = 0.833, xmax = 1.899),
+                   fill = "red",
+                   color = "red",
+                   stroke = 1,
+                   size = 1.5,
+                   alpha = 0.3) + 
+  geom_count(data = model_rejection_nl$data,
+             shape = 21,
+             fill = "#22BDAD",
+             color = "black") +
+  scale_x_continuous(breaks = seq(0, 2.4, 0.1)) +
+  scale_y_continuous(breaks = seq(0, 1, 0.1)) +
+  xlab("Inter-square distance (mm)") +
+  ylab("Rejection Probability") +
+  labs(size = "Number of Trials") + 
+  theme_bw() +
+  theme(
+    legend.position = "right",
+    legend.title = element_text(size = 12),
+    axis.text.x = element_text(size = 8),
+    axis.text.y = element_text(size = 8),
+    axis.title.x = element_text(size = 12, face = "bold"),
+    axis.title.y = element_text(size = 12, face = "bold"),
+    # text = element_text(family = "Courier New"),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank()
+  ) 
+
+## Save plot
+ggsave("Figures/rejection_probability_nl.png",
+       plot = plot,
+       width = 7.5,
+       height = 4,
+       units = "in",
+       dpi = 600
+)
